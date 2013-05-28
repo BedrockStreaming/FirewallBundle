6d86cc099b072071087ce58f3b02fa53afaa3d9b
Adrien SAMSON <asamson.externe@m6.fr>
2013-05-28 15:54:41 +0200

    Decoupled unit tests

---
+++ Tests/Units/DependencyInjection/FirewallExtension.php
@@ -0,0 +1,131 @@
+<?php
+namespace M6Web\Bundle\FirewallBundle\Tests\Units\DependencyInjection;
+
+require_once __DIR__.'/../../bootstrap.php';
+
+use atoum\AtoumBundle\Test\Units;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use M6Web\Bundle\FirewallBundle\DependencyInjection\FirewallExtension as FirewallExt;
+
+/**
+ * Test of the FirewallBundle extension declaration
+ */
+class FirewallExtension extends Units\Test
+{
+    protected $configs = array(
+        array(
+           'configs' => array(
+                'default' => array(
+                    'default_state' => true,
+                    'error_code'    => 400,
+                    'error_message' => 'Interdit',
+                    'throw_error'   => false,
+                ),
+            ),
+            'lists' => array(
+                'partners' => array(
+                    '10.20.30.40',
+                    '40.30.20.10',
+                ),
+            ),
+        ),
+        array(
+            'configs' => array(
+                'default' => array(
+                    'lists'   => array(
+                        'default'  => true,
+                        'partners' => false,
+                    ),
+                    'entries' => array(
+                        '127.0.0.1'   => true,
+                        '192.168.1.1' => false,
+                    ),
+                ),
+            ),
+            'lists' => array(
+                'default' => array(
+                    '::1',
+                    '192.168.1.*',
+                    '192.168.0.0-192.168.0.254',
+                ),
+            ),
+        )
+    );
+
+    /**
+     * Configuration load test
+     */
+    public function testLoad()
+    {
+        $container = new ContainerBuilder();
+
+        $loader = new FirewallExt();
+        $loader->load($this->configs, $container);
+
+        $this->assert
+            ->string($container->getParameter('m6.firewall.class'))
+                ->isEqualTo('M6Web\Bundle\FirewallBundle\Firewall\Firewall')
+            ->array($container->getParameter('m6.firewall.lists'))
+                ->isEqualTo(array(
+                    'default' => array(
+                        '::1',
+                        '192.168.1.*',
+                        '192.168.0.0-192.168.0.254',
+                    ),
+                    'partners' => array(
+                        '10.20.30.40',
+                        '40.30.20.10',
+                    ),
+                ))
+            ->array($container->getParameter('m6.firewall.configs'))
+                ->isEqualTo(array(
+                    'default' => array(
+                        'default_state' => true,
+                        'error_code'    => 400,
+                        'error_message' => 'Interdit',
+                        'throw_error'   => false,
+                        'lists'   => array(
+                            'default'  => true,
+                            'partners' => false,
+                        ),
+                        'entries' => array(
+                            '127.0.0.1'   => true,
+                            '192.168.1.1' => false,
+                        ),
+                    ),
+                ))
+
+            ->object($providerDefinition = $container->getDefinition('m6.firewall.provider'))
+            ->string($providerDefinition->getClass())
+                ->isEqualTo('M6Web\Bundle\FirewallBundle\Firewall\Provider')
+            ->object($providerDefinition->getArgument(0))
+                ->isInstanceOf('Symfony\Component\DependencyInjection\Reference')
+            ->string((string) $providerDefinition->getArgument(0))
+                ->isEqualTo('service_container')
+            ->string($providerDefinition->getArgument(1))
+                ->isEqualTo('%m6.firewall.lists%')
+            ->string($providerDefinition->getArgument(2))
+                ->isEqualTo('%m6.firewall.configs%')
+            ->array($providerDefinition->getMethodCalls())
+                ->hasSize(1)
+            ->array($providerDefinition->getMethodCalls()[0])
+                ->isEqualTo(array('setFirewallClass', array('%m6.firewall.class%')))
+
+            ->object($listenerDefinition = $container->getDefinition('m6.firewall.controller_listener'))
+            ->string($listenerDefinition->getClass())
+                ->isEqualTo('M6Web\Bundle\FirewallBundle\Controller\Listener')
+            ->object($listenerDefinition->getArgument(0))
+                ->isInstanceOf('Symfony\Component\DependencyInjection\Reference')
+            ->string((string) $listenerDefinition->getArgument(0))
+                ->isEqualTo('annotation_reader')
+            ->object($listenerDefinition->getArgument(1))
+                ->isInstanceOf('Symfony\Component\DependencyInjection\Reference')
+            ->string((string) $listenerDefinition->getArgument(1))
+                ->isEqualTo('m6.firewall.provider')
+            ->array($listenerDefinition->getTag('kernel.event_listener'))
+                ->isEqualTo(array(array('event' => 'kernel.controller', 'method' => 'onCoreController', 'priority' => -255)))
+            ;
+    }
+}
--- Tests/Units/Firewall/Provider.php
+++ Tests/Units/Firewall/Provider.php
@@ -12,11 +12,9 @@ use M6Web\Bundle\FirewallBundle\Firewall;
  */
 class Provider extends Units\Test
 {
-    protected static $request;
-    protected static $container;
-    protected static $provider;
+    protected $request;
 
-    public static $configs = array(
+    protected $configs = array(
         'default' => array(
             'default_state' => true,
             'error_code'    => 400,
@@ -31,9 +29,37 @@ class Provider extends Units\Test
                 '192.168.1.1' => false,
             ),
         ),
+        'empty' => array(
+            'toomuch' => true,
+        ),
     );
 
-    public static $lists = array(
+    protected $expectedConfig = array(
+        'default' => array(
+            'default_state' => true,
+            'error_code'    => 400,
+            'error_message' => 'Interdit',
+            'throw_error'   => false,
+            'lists'   => array(
+                'default'  => true,
+                'partners' => false,
+            ),
+            'entries' => array(
+                '127.0.0.1'   => true,
+                '192.168.1.1' => false,
+            ),
+        ),
+        'empty' => array(
+            'default_state' => null,
+            'error_message' => null,
+            'error_code' => null,
+            'throw_error' => null,
+            'lists' => null,
+            'entries' => array(),
+        ),
+    );
+
+    protected $lists = array(
         'default' => array(
             '::1',
             '192.168.1.*',
@@ -45,7 +71,7 @@ class Provider extends Units\Test
         ),
     );
 
-    protected static $options = array(
+    protected $options = array(
         'default_state' => false,
         'error_message' => 'TestOptions',
     );
@@ -57,8 +83,12 @@ class Provider extends Units\Test
     {
         $provider = $this->getProvider();
 
-        self::assertListsMatch($this, $provider->getLists());
-        self::assertConfigsMatch($this, $provider->getConfigs());
+        $this->assert
+            ->array($provider->getLists())
+                ->isEqualTo($this->lists)
+            ->array($provider->getConfigs())
+                ->isEqualTo($this->expectedConfig)
+        ;
     }
 
     /**
@@ -69,7 +99,7 @@ class Provider extends Units\Test
         $provider = $this->getProvider();
         $provider->setFirewallClass('\mock\M6Web\Bundle\FirewallBundle\Firewall\FirewallInterface');
 
-        $firewall = $provider->getFirewall('default', self::$options);
+        $firewall = $provider->getFirewall('default', $this->options);
 
         $this
             ->mock($firewall)
@@ -77,100 +107,33 @@ class Provider extends Units\Test
                     ->withIdenticalArguments($provider)
                     ->once()
                 ->call('setRequest')
-                    ->withIdenticalArguments($this->getRequest())
+                    ->withIdenticalArguments($this->request)
                     ->once()
                 ->call('setDefaultState')
-                    ->withIdenticalArguments(self::$options['default_state'])
+                    ->withIdenticalArguments($this->options['default_state'])
                     ->once()
                 ->call('setErrorMessage')
-                    ->withIdenticalArguments(self::$options['error_message'])
+                    ->withIdenticalArguments($this->options['error_message'])
                     ->once()
         ;
-    }
-
-    /**
-     * Get a request mock
-     *
-     * @return \Mock\Request
-     */
-    public function getRequest()
-    {
-        if (!self::$request) {
-            $this->mockClass('Symfony\Component\HttpFoundation\Request', '\Mock');
-
-            self::$request = new \Mock\Request();
-        }
-
-        return self::$request;
-    }
-
-    /**
-     * Get a container mock
-     *
-     * @return \Mock\ContainerInterface
-     */
-    public function getContainer()
-    {
-        if (!self::$container) {
-            $this->mockClass('Symfony\Component\DependencyInjection\ContainerInterface', '\Mock');
-
-            self::$container = new \Mock\ContainerInterface();
-        }
-
-        return self::$container;
     }
 
     /**
      * Get a provider mock
      *
-     * @return \Mock\Provider
+     * @return Firewall\Provider
      */
     protected function getProvider()
     {
-        if (!self::$provider) {
-            $container = $this->getContainer();
-            $request = $this->getRequest();
-            $container->getMockController()->get = function($serviceName) use ($request) {
-                switch ($serviceName) {
-                    case 'request':
-                        return $request;
-                }
-            };
-            self::$provider = new Firewall\Provider($container, self::$lists, self::$configs);
-        }
-
-        return self::$provider;
-    }
-
-    /**
-     * Static assertion of lists matching
-     *
-     * @param Units\Test $class Test instance
-     * @param array      $lists Lists to test
-     */
-    public static function assertListsMatch(Units\Test $class, $lists)
-    {
-        $class->assert
-            ->array(self::$lists)
-                ->isEqualTo($lists)
-        ;
-    }
-
-    /**
-     * Static assertion of configuration matching
-     *
-     * @param Units\Test $class   Test instance
-     * @param array      $configs Configurations to test
-     */
-    public static function assertConfigsMatch(Units\Test $class, $configs)
-    {
-        foreach (self::$configs as $configName => $params) {
-            foreach ($params as $paramName => $param) {
-                $class->assert
-                    ->variable($param)
-                        ->isEqualTo($configs[$configName][$paramName])
-                ;
+        $container = new \Mock\Symfony\Component\DependencyInjection\ContainerInterface();
+        $this->request = new \Mock\Symfony\Component\HttpFoundation\Request();
+        $container->getMockController()->get = function($serviceName) {
+            switch ($serviceName) {
+                case 'request':
+                    return $this->request;
             }
-        }
+        };
+
+        return new Firewall\Provider($container, $this->lists, $this->configs);
     }
 }
